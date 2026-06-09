7ebbfa34f9c3ca9dd99ff5667f6c1276888dd512
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-09 14:03:50 +0530

    Update

--- src/main/java/SMS/StudentDAOImpl.java
+++ src/main/java/SMS/StudentDAOImpl.java
@@ -1,8 +1,9 @@
 package SMS;
 
-import java.sql.*;
-public class StudentDAOImpl implements StudentDAO{
+import com.mysql.cj.protocol.Resultset;
 
+import java.sql.*;
+public class StudentDAOImpl implements StudentDAO{A
     @Override
     public void addStudent(Student s){      // add_Student;
 
@@ -29,5 +30,45 @@ public class StudentDAOImpl implements StudentDAO{
     }
 
 
+
+    // ---------:View Student:-----------
+
+    @Override
+    public void viewStudent(){
+
+         try{
+             Connection con = DBConnection.getConnection();
+
+
+             PreparedStatement ps = con.prepareStatement("SELECT *FROM student");
+
+             ResultSet rs = ps.executeQuery();
+
+             while(rs.next()){
+                 System.out.println(
+                         rs.getInt(1)+ " " +
+                                 rs.getString(2)+ " " +
+                                 rs.getString(3)
+
+
+                 );
+             }
+
+             con.close();
+
+         }
+          catch(Exception e){
+
+             e.printStackTrace();
+          }
+    }
+
+
+    //:-----------------------------------------Search Student:---------------------------------------------------:
+
+    @Override
+    public void searchStudent(int id){
+
+    }
 
 }
